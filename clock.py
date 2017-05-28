49866d3e38471b56c845d6eaa2776adc280cbc95
EGG <emmigunn@gmail.com>
2017-05-28 02:37:49 +0000

    update

--- clock.py
+++ clock.py
@@ -1,11 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import sys
 import runpy
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    runpy.run_path("manage.py getdata")
+    sys.argv = ['', 'getdata']
+    runpy.run_path('manage.py')
 
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
