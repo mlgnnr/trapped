6c9c852a7a2be3148e6cf7cd0c3075e7681e7a04
EGG <emmigunn@gmail.com>
2017-05-28 16:32:36 +0000

    update

--- first/local_settings.py
+++ first/local_settings.py
@@ -3,9 +3,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'heidin',
+		'USER': 'heidinuser',
+		'PASSWORD': 'floki',
+		'HOST': 'localhost',
+		'PORT': '',
+	}
 }
 
 DEBUG = True
