53d7263445f4d488d09e6325c9833eceb10ca65e
EGG <emmigunn@gmail.com>
2017-05-28 13:01:06 +0000

    models update

--- first/settings.py
+++ first/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
