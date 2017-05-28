4f7091098aa863fe09d0761c455e4f9e2ed18ac6
EGG <emmigunn@gmail.com>
2017-05-28 01:59:21 +0000

    db

--- first/settings.py
+++ first/settings.py
@@ -75,6 +75,12 @@ WSGI_APPLICATION = 'first.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    }
+}
+
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
