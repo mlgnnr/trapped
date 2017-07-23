660d35b635cec66d36d41280244b6111c9d91c7f
Emil Gunnar Gunnarsson <mlgnnr@Emils-MacBook-Pro.local>
2017-07-23 00:42:52 +0000

    changes to api logic

--- cond/management/commands/getdata.py
+++ cond/management/commands/getdata.py
@@ -11,6 +11,8 @@ class Command(BaseCommand):
     help = 'gets data from vegagerdin'
 
     def handle(self, *args, **options):
-        Api.getForecast()
+
+        Api.addDataToDataBase()
+        # Api.getForecast()
         # Api.getRoads()
         # Api.getCurrentWeather()
--- cond/utility/api.py
+++ cond/utility/api.py
@@ -3,19 +3,20 @@ import csv
 import codecs
 from datetime import datetime
 import xml.etree.ElementTree as etree
-from cond.models import Road, WeatherStation, WeatherForecast
+from cond.models import Road, WeatherStation, WeatherForecast, Condition
 from django.utils import timezone
 from cond.utility.date import DateUtility
+from enum import Enum
 import logging
 
 logger = logging.getLogger(__name__)
 
-
 class Api():
 
     # road_id_dir = {'hellisheidi_id': 902020003, 'threngslin_id': 902340003}
-    road_id_dir = {'sandskeid_id': 903260003, 'hellisheidi_id': 902020003,
-                   'threngslin_id': 902340003}
+    road_id_dir = {'Sandskeið': 903260003, 'Hellisheiði': 902020003,
+                   'Þrengslin': 902340003}
+
     station_id_dir = {'sandskeid_id': 17, 'hellisheidi_id': 1,
                       'threngslin_id': 31}
 
@@ -28,8 +29,26 @@ class Api():
     threngslin_img = "http://www.vegagerdin.is/vgdata/vefmyndavelar/threngsli_1.jpg"
     sandskeid_img = "http://www.vegagerdin.is/vgdata/vefmyndavelar/sandskeid_1.jpg"
 
+    def addDataToDataBase():
+        for road_name, road_id in Api.road_id_dir.items():
+            road = (road_name, road_id)
+            Api.databaseContainsRoad(road)
+            # Api.databaseContainsRoad(road_id)
+        Api.getRoadCondition()
+        # Api.getCurrentWeather()
+        # Api.getForecast()
+        # Api.getWebCams()
+
+    def databaseContainsRoad(road):
+        try:
+            check_road = Road.objects.get(id_butur=road[1])
+        except Road.DoesNotExist:
+            print("Not in database: " + road[0])
+            new_road = Road(name=road[0],id_butur=road[1],last_updated=timezone.now())
+            new_road.save()
+
     def makeRequest(type):
-        if(type == "roads"):
+        if(type == "condition"):
             r = requests.get('http://gagnaveita.vegagerdin.is/api/faerd2014_1')
         elif(type == "weather"):
             r = requests.get('http://gagnaveita.vegagerdin.is/api/vedur2014_1')
@@ -37,10 +56,10 @@ class Api():
             r = requests.get('http://xmlweather.vedur.is/?op_w=xml&type=forec&lang=is&view=csv&ids=31392')
         return r
 
-    def getRoads():
-        r = Api.makeRequest("roads")
+    def getRoadCondition():
+        r = Api.makeRequest("condition")
         if(r.status_code == 200):
-            Api.parseRoad(r.json())
+            Api.parseRoadCondition(r.json())
         else:
             print("Error retrieving Condition from website")
 
@@ -69,24 +88,27 @@ class Api():
         result = []
         number_of_forecasts_added = 0
         found_currentHour = False
-        road = Road.objects.get(name="Hellisheiði")
+        road = Road.objects.get(name="Hellieiði")
+        print("Hallo")
         road.weather.weatherforecast_set.all().delete()
+        print(road)
 
-        for forecast in forecasts:
-            datetime_object = DateUtility.makeDateObject(forecast['Spátími'])
 
-            if not(found_currentHour):
-                if(DateUtility.sameDay(datetime_object) and DateUtility.sameHour(datetime_object)):
-                    print('correct hour and day')
-                    found_currentHour = True
-                    number_of_forecasts_added += 1
-                    Api.saveForecastObject(forecast)
-            elif (found_currentHour and number_of_forecasts_added <= number_of_hours):
-                number_of_forecasts_added += 1
-                Api.saveForecastObject(forecast)
-
-            if number_of_forecasts_added > number_of_hours:
-                break
+        # for forecast in forecasts:
+        #     datetime_object = DateUtility.makeDateObject(forecast['Spátími'])
+        #
+        #     if not(found_currentHour):
+        #         if(DateUtility.sameDay(datetime_object) and DateUtility.sameHour(datetime_object)):
+        #             print('correct hour and day')
+        #             found_currentHour = True
+        #             number_of_forecasts_added += 1
+        #             Api.saveForecastObject(forecast)
+        #     elif (found_currentHour and number_of_forecasts_added <= number_of_hours):
+        #         number_of_forecasts_added += 1
+        #         Api.saveForecastObject(forecast)
+        #
+        #     if number_of_forecasts_added > number_of_hours:
+        #         break
             #
             # if (DateUtility.getDay(datetime_object) == DateUtility.getCurrentDay() and not(found_currentHour)):
             #     print('correct day')
@@ -130,15 +152,15 @@ class Api():
             for station_id in Api.station_id_dir.values():
                 Api.searchWeatherStation(station, station_id)
 
-    def parseRoad(response):
+    def parseRoadCondition(response):
         for road in response:
             for road_id in Api.road_id_dir.values():
                 Api.search_road(road, road_id)
 
-    def search_road(road, road_id):
-        if road['IdButur'] == road_id and road['IdLeid'] is not None:
-            print(road['IdLeid'])
-            Api.updateRoadObject(road, road_id)
+    def search_road(response_road, road_id):
+        if response_road['IdButur'] == road_id and response_road['IdLeid'] is not None:
+            print(response_road['IdLeid'])
+            Api.updateRoadConditionObject(response_road)
 
     def searchWeatherStation(station, station_id):
         if station['Nr'] == station_id:
@@ -150,13 +172,26 @@ class Api():
             print(station['Maelist_nr'])
 
 
-    def updateRoadObject(new_road, road_id):
-        if road_id == Api.road_id_dir['hellisheidi_id']:
-            Api.saveRoadObject(1, new_road)
-        elif road_id == Api.road_id_dir['threngslin_id']:
-            Api.saveRoadObject(2, new_road)
-        elif road_id == Api.road_id_dir['sandskeid_id']:
-            Api.saveRoadObject(3, new_road)
+    def updateRoadConditionObject(new_road):
+
+        road = Road.objects.get(id_butur=new_road['IdButur'])
+        try:
+            condition = road.condition
+        except Condition.DoesNotExist:
+            new_condition = Condition(road=road,
+                                      status=new_road['StuttAstand'],
+                                      sign=new_road['Skilti'])
+            new_condition.save()
+
+
+
+        # Api.saveRoadConditionObject(new_road)
+        # if road_id == Api.road_id_dir['hellisheidi_id']:
+        #     Api.saveRoadObject(1, new_road)
+        # elif road_id == Api.road_id_dir['threngslin_id']:
+        #     Api.saveRoadObject(2, new_road)
+        # elif road_id == Api.road_id_dir['sandskeid_id']:
+        #     Api.saveRoadObject(3, new_road)
 
     def updateWeatherStationObject(new_station, station_id):
         if station_id == Api.station_id_dir['hellisheidi_id']:
@@ -166,7 +201,7 @@ class Api():
         elif station_id == Api.station_id_dir['sandskeid_id']:
             Api.saveWeatherStationObject(3, new_station)
 
-    def saveRoadObject(pk, new_road):
+    def saveRoadConditionObject(new_road):
             road = Road.objects.get(pk=pk)
             print(road)
             print("Old condition: " + road.condition)
