e0daf118dc01915ef16eb4608f814c2c7f69545e
Emil Gunnar Gunnarsson <mlgnnr@Emils-MacBook-Pro.local>
2017-07-30 00:59:51 +0000

    added heroku db

--- first/settings.py
+++ first/settings.py
@@ -75,14 +75,20 @@ WSGI_APPLICATION = 'first.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "mlgnnr",
+#         "USER": "mlgnnr",
+#         "PASSWORD": "",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "mlgnnr",
-        "USER": "mlgnnr",
-        "PASSWORD": "",
-        "HOST": "localhost",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
     }
 }
 
