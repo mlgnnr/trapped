8b170e90c1c47e640f41baf1ac67d92089cfbac8
mlgnnr <emmigunn@gmail.com>
2018-07-31 01:10:52 +0000

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -34,6 +34,7 @@ class ResultsView(generic.DetailView):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 #     context = {'latest_question_list': latest_question_list}
 #     return render(request, 'polls/index.html', context)
+# blaba
 
 
 # def detail(request, question_id):
